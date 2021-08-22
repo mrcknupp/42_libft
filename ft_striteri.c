55b527289fcd675837e33dfefecc2fe28dfe62f1
Marcos Rodrigo <marcosrck@gmail.com>
2021-08-22 01:00:46 -0300

    fix bugs

--- ft_striteri.c
+++ ft_striteri.c
@@ -16,8 +16,12 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
+	if (!s || !f)
+	{
+		return ;
+	}
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		f(i, s + i);
 		i++;
