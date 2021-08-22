3311cee74d23b4a6e3e000a3f72891028be3c7a3
Marcos Rodrigo <marcosrck@gmail.com>
2021-08-22 00:45:50 -0300

    fix bugs

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,10 +14,17 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
-		if (*s++ == (char)c)
-			return ((char *)s - 1);
-	if (c == '\0')
+	while (*(char *)s != '\0')
+	{
+		if (*(char *)s == (char)c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	if (*(char *)s == (char)c)
+	{
 		return ((char *)s);
+	}
 	return (NULL);
 }
